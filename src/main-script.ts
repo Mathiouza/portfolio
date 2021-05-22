2f9cccc1082c0f860123ea48cf8475cc8d068a34
Mathieu Casubolo <mathieu.casubolo@polymtl.ca>
2021-05-22 16:34:35 -0400

    Add about page

--- src/main-script.ts
+++ src/main-script.ts
@@ -1,4 +1,64 @@
+function showAbout() {
 
+    document.getElementById("space-wave").style.display = "unset";
+
+    setTimeout(() => {
+
+        document.getElementById("photo-space-wave").style.display = "none";
+
+    }, 1800)
+
+    setTimeout(() => {
+
+        document.getElementById("space-wave").style.display = "none";
+
+    }, 1200)
+
+    setTimeout(() => {
+
+        document.getElementById("photo-main-wave").className = "wave photo-wave-step2";
+
+    }, 800)
+
+    setTimeout(() => {
+
+        document.getElementById("photo-space-wave").className = "wave photo-wave-step2";
+
+    }, 700)
+
+    setTimeout(() => {
+
+        document.getElementById("about").className = "about-showed";
+
+    }, 600)
+
+    setTimeout(() => {
+
+        document.getElementById("presentation").className = "presentation-hidden";
+
+    }, 500)
+
+    setTimeout(() => {
+
+        document.getElementById("main-wave").className = "wave wave-step3";
+
+    }, 200)
+
+    setTimeout(() => {
+
+        document.getElementById("space-wave").className = "wave wave-step3";
+
+    }, 100)
+
+    setTimeout(() => {
+
+        document.getElementById("sec-wave").className = "wave wave-step3";
+
+    }, 0)
+    
+}
+
+document.getElementById("about-button").addEventListener("click", showAbout)
 
 window.onload = () => {
 
@@ -10,8 +70,14 @@ window.onload = () => {
 
     setTimeout(() => {
 
+        document.getElementById("navigation").style.opacity = "100%";
+
+    }, 500)
+
+    setTimeout(() => {
+
         document.getElementById("main-wave").className = "wave wave-step2";
-        window.scrollTo(0, 0)
+        window.scrollTo(0,0);
 
     }, 200)
 
@@ -31,12 +97,27 @@ window.onload = () => {
 }
 
 window.onscroll = () => {
+    window.scrollTo(0,0);
+}
 
-    const factor = 100*window.scrollY/window.innerHeight;
+let navButtons = document.getElementsByClassName("nav-text");
+let navArrows = document.getElementsByClassName("nav-arrow");
 
-    if(factor < 100)
-        document.getElementById("page-container").style.opacity = factor+"%";
-    else
-        document.getElementById("page-container").style.opacity = "100%";
+for(let i = 0 ; i < navButtons.length ; i++) {
+
+    let b = <HTMLDivElement> navButtons[i];
+    let a = <HTMLDivElement> navArrows[i];
+
+    b.onmouseenter = () => {
+
+        a.className = "nav-arrow rotate-nav-arrow";
+
+    }
+
+    b.onmouseleave = () => {
+
+        a.className = "nav-arrow";
+
+    }
 
 }
